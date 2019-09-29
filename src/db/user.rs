460b39c23ab26bce0733c2a23c8a7efa46c667c9
nhyne <nhyne@nhyne.dev>
2019-09-29 14:04:24 -0400

    properly encrypting passwords

--- src/db/user.rs
+++ src/db/user.rs
@@ -2,33 +2,35 @@ extern crate rand;
 extern crate scrypt;
 
 use rand::prelude::*;
-use scrypt::{ScryptParams, scrypt_simple};
+use scrypt::{ScryptParams, scrypt_simple, scrypt_check};
+use crate::db::schema::users;
 
+#[derive(Insertable)]
+#[table_name = "users"]
 pub struct NewUser {
     pub email: String,
-    pub hashed_password: String,
+    pub password: String,
     pub username: String,
     salt: String,
 }
 
 impl NewUser {
-    pub fn new(email: String, raw_password: &str, username: String) -> NewUser {
+    pub fn new(email: String, raw_password: String, username: String) -> NewUser {
         let salt : String = rand::thread_rng()
-            .gen_ascii_chars()
-            .take(10)
+            .sample_iter(rand::distributions::Alphanumeric)
+            .take(50)
             .collect();
-        println!("salt: {}", salt);
 
         let params = ScryptParams::new(15, 8, 1).unwrap();
-        let hashed_password = scrypt_simple(raw_password, &params)
+        let salt_combination = format!("{}{}", salt, raw_password);
+        let password = scrypt_simple(&salt_combination, &params)
             .expect("OS RNG should not fail");
 
-        println!("hashed pass: {}", hashed_password);
         NewUser {
             email,
             username,
             salt,
-            hashed_password,
+            password,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+#![feature(proc_macro_hygiene)]
+// Rocket web server
+#![feature(decl_macro)]
 mod db;
 use db::user::NewUser;
 
+// Diesel ORM
+#[macro_use]
+extern crate diesel;
+
 fn main() {
-    println!("Hello, world!");
-    NewUser::new(String::from("something@somewhere.io"), "abadpass", String::from("username"));
+    NewUser::new(String::from("something@somewhere.io"), String::from("abadpass"), String::from("username"));
 }
