882e07448f333f7bda6e59c0653e79f6bcc779a7
nhyne <nhyne@nhyne.dev>
2019-11-24 14:54:03 -0500

    changing formatting script

--- scripts/ci/format.sh
+++ scripts/ci/format.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
 cargo fmt
-CHANGED=$(git status -s)
-if [[ -z "$CHANGED" ]]; then
+git diff-files --quiet
+CHANGED_RC=$?
+if [[ "$CHANGED_RC" -eq 1 ]]; then
     echo "Working tree is dirty after formatting"
     exit 1
 fi
