688e2b6059d6aa38ee5fd2e69f7d54402e146d7c
nhyne <nhyne@nhyne.dev>
2019-11-25 08:46:44 -0500

    not running clippy on every push, takes too long

--- scripts/ci/format.sh
+++ scripts/ci/format.sh
@@ -1,13 +1,5 @@
 #! /usr/bin/env bash
 
-cargo fmt
-git diff-files --quiet
-CHANGED_RC=$?
-if [[ "$CHANGED_RC" -eq 1 ]]; then
-    echo "Working tree is dirty after formatting"
-    exit 1
-fi
-
-cargo clippy -- -D warnings
+cargo fmt --all -- --check
 
 echo "Formatting complete"
